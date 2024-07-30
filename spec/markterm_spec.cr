dcbe4f8ad1abdca9ed1b687d504d52f770af043e
Roberto Alsina <roberto.alsina@gmail.com>
2024-07-30 17:46:55 -0300

    Beginnings of things

--- spec/markterm_spec.cr
+++ spec/markterm_spec.cr
@@ -1,9 +1,45 @@
 require "./spec_helper"
 
-describe Markterm do
-  # TODO: Write tests
+basic_style = Markd::Style.new
+basic_style.fore = :white
+basic_style.back = :black
+basic_style.underline = false
+basic_style.bold = false
+basic_style.italic = false
 
-  it "works" do
-    false.should eq(true)
+fore_only = Markd::Style.new
+fore_only.fore = :red
+
+describe "Markterm" do
+  describe "Style" do
+    it "adds" do
+      new_s = basic_style + fore_only
+      new_s.fore.should eq(:red)
+      new_s.back.should eq(:black)
+    end
+  end
+  describe "StyleStack" do
+    it "works" do
+      stack = Markd::StyleStack.new
+      stack << basic_style
+      stack.last.fore.should eq(:white)
+      stack.last.back.should eq(:black)
+      stack.last.underline.should eq(false)
+    end
+
+    it "applies styles" do
+      stack = Markd::StyleStack.new
+      stack << basic_style
+      stack.apply("test").to_s.should eq(
+        "test".colorize.fore(:white).back(:black).to_s)
+    end
+
+    it "merges partial styles" do
+      stack = Markd::StyleStack.new
+      stack << basic_style
+      stack << fore_only
+      stack.current.fore.should eq(:red)
+      stack.current.back.should eq(:black)
+    end
   end
 end
--- src/markterm.cr
+++ src/markterm.cr
@@ -1,3 +1,4 @@
+require "markd"
 require "colorize"
 
 macro def_method(name)
@@ -9,7 +10,65 @@ macro def_method(name)
 end
 
 module Markd
+  struct Style
+    property bold : Bool? = nil
+    property italic : Bool? = nil
+    property underline : Bool? = nil
+    property fore : Symbol? = nil
+    property back : Symbol? = nil
+
+    def +(other : Style) : Style
+      new = Style.new
+      new.bold = other.bold.nil? ? self.bold : other.bold
+      new.underline = other.underline.nil? ? self.underline : other.underline
+      new.italic = other.italic.nil? ? self.italic : other.italic
+      new.fore = other.fore.nil? ? self.fore : other.fore
+      new.back = other.back.nil? ? self.back : other.back
+      new
+    end
+  end
+
+  struct StyleStack
+    @stack : Array(Style) = [] of Style
+
+    def current : Style
+      @stack.reduce(@stack[0]) { |acc, i| acc + i }
+    end
+
+    def apply(input : String)
+      style = current
+      input = input.colorize.fore(style.fore.as(Symbol)) \
+        .back(style.back.as(Symbol))
+      input = input.bold if style.bold
+      input = input.underline if style.underline
+      input = input.italic if style.italic
+      input
+    end
+
+    def <<(style : Style)
+      @stack << style
+    end
+
+    def last : Style
+      @stack.last
+    end
+  end
+
   class TermRenderer < Renderer
+    @style : StyleStack = StyleStack.new
+
+    def initialize(@options = Options.new)
+      @output_io = String::Builder.new
+      @last_output = "\n"
+      s = Style.new
+      s.fore = :green
+      s.back = :black
+      s.underline = false
+      s.bold = false
+      s.italic = false
+      @style << s
+    end
+
     def_method block_quote
     def_method code
     def_method code_block
@@ -20,17 +79,28 @@ module Markd
     def_method image
     def_method item
     def_method line_break
-    def_method link
+
+    def link(node : Node, entering : Bool)
+      return unless entering
+      s = Style.new
+      s.underline = true
+      s.fore = :blue
+      @style << s
+    end
+
     def_method list
     def_method paragraph
     def_method soft_break
     def_method strong
-    def_method text
+
+    def text(node : Node, entering : Bool)
+      @output_io << @style.apply node.text
+    end
+
     def_method thematic_break
   end
 
   def self.to_term(source : String, options = Options.new) : String
-    p! "to_term"
     return "" if source.empty?
     document = Parser.parse(source, options)
     renderer = TermRenderer.new(options)
