cc964e50a736869372779d56398688db83e040fb
Roberto Alsina <roberto.alsina@gmail.com>
2024-07-31 09:58:18 -0300

    Code to detect dark and light terminals

---
+++ src/terminal.cr
@@ -0,0 +1,60 @@
+module Terminal
+  extend self
+
+  def is_terminal_light : Bool
+    # If the COLORFGBG environment variable is set, we can use
+    # it to determine the result. It will be something like
+    # `15;0` or `0;15`. The first number is the foreground color
+    # and the second number is the background color.
+    if ["15;0", "15;default;0"].includes?(ENV.fetch("COLORFGBG", ""))
+      return true
+    end
+
+    # In most linuxy terminals, we can query the terminal's
+    # background color by sending an escape sequence.
+    # We should only try if we are in a tty
+    # and the TERM environment variable is set.
+    if !ENV.fetch("TERM", "").empty? && STDOUT.tty? && STDIN.tty?
+      bg = parse_color(query_terminal("11"))
+      fg = parse_color(query_terminal("10"))
+
+      if bg.nil? # Who knows!
+        return false
+      end
+
+      # Some terms (alacritty) don't support querying the
+      # fg color, so we just guess based on the bg color
+      if bg == fg || !fg
+        return bg.sum > 384  # Quick and dirty brightness check
+      end
+
+      return fg.sum < bg.sum # FG is darker, so term is light
+    end
+    # Let's just assume it's dark
+    false
+  end
+
+  def parse_color(color)
+    m = /([0-9a-f]+)\/([0-9a-f]+)\/([0-9a-f]+)/.match(color)
+    if m.nil?
+      return nil
+    end
+    r = m[0][...2].to_i(16)
+    g = m[1][...2].to_i(16)
+    b = m[2][...2].to_i(16)
+    [r, g, b]
+  end
+
+  def query_terminal(color) : String
+    STDOUT << "\e]#{color};?\e\\"
+    STDOUT.flush
+    result = String::Builder.new
+    STDIN.raw do |io|
+      io.each_char do |c|
+        break if c == "\a" || c == '\\'
+        result << c
+      end
+    end
+    result.to_s
+  end
+end
