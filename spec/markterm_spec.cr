824fc1dd46387c8653b5bf0f31d4e94d15f5f718
Roberto Alsina <roberto.alsina@gmail.com>
2024-07-31 21:31:56 -0300

    Tests

--- spec/markterm_spec.cr
+++ spec/markterm_spec.cr
@@ -1,16 +1,46 @@
 require "./spec_helper"
 
-basic_style = Markd::Style.new
+basic_style = Terminal::Style.new
 basic_style.fore = :white
 basic_style.back = :black
 basic_style.underline = false
 basic_style.bold = false
 basic_style.italic = false
 
-fore_only = Markd::Style.new
+fore_only = Terminal::Style.new
 fore_only.fore = :red
 
 describe "Markterm" do
+  describe "TermRenderer" do
+    it "works" do
+      Markd.to_term("text").should eq("  text")
+    end
+    it "does block quotes" do
+      Markd.to_term("> text\n> text2").should eq(
+        "\n" + "  │ \e[37;3mtext\e[0m\n" + "  │ \e[37;3mtext2\e[0m\n"
+      )
+    end
+    it "does links with just a URL" do
+      Markd.to_term("<http://go.to>").should eq(
+        "  \e[34;4m<http://go.to>\e[0m"
+      )
+    end
+    it "does links with text" do
+      Markd.to_term("[foo](http://go.to)").should eq(
+        "  \e[34;4mfoo \e[0m\e[34;4m<http://go.to>\e[0m"
+      )
+    end
+    it "removes text if it's just the URL" do
+      Markd.to_term("[http://go.to](http://go.to)").should eq(
+        "  \e[34;4m<http://go.to>\e[0m"
+      )
+    end
+    pending "uses OSC 8 for links when TERM is kitty" do
+      Markd.to_term("[foo](http://go.to)").should eq(
+        "  \e]8;;https://go.to\e\\foo\e]8;;\e\\"
+      )
+    end
+  end
   describe "Style" do
     it "adds" do
       new_s = basic_style + fore_only
@@ -20,7 +50,7 @@ describe "Markterm" do
   end
   describe "StyleStack" do
     it "works" do
-      stack = Markd::StyleStack.new
+      stack = Terminal::StyleStack.new
       stack << basic_style
       stack.last.fore.should eq(:white)
       stack.last.back.should eq(:black)
@@ -28,14 +58,14 @@ describe "Markterm" do
     end
 
     it "applies styles" do
-      stack = Markd::StyleStack.new
+      stack = Terminal::StyleStack.new
       stack << basic_style
       stack.apply("test").to_s.should eq(
         "test".colorize.fore(:white).back(:black).to_s)
     end
 
     it "merges partial styles" do
-      stack = Markd::StyleStack.new
+      stack = Terminal::StyleStack.new
       stack << basic_style
       stack << fore_only
       stack.current.fore.should eq(:red)
