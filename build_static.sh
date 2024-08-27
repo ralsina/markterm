b0820216c0c610a6292b772df596725531890152
Roberto Alsina <roberto.alsina@gmail.com>
2024-08-27 14:41:01 -0300

    bump: Release v0.3.2

--- build_static.sh
+++ build_static.sh
@@ -7,10 +7,10 @@ docker run --rm --privileged \
 
 # Build for AMD64
 docker build . -f Dockerfile.static -t markterm-builder
-docker run -ti --rm -v "$PWD":/app --user="$UID" markterm-builder /bin/sh -c "cd /app && rm -rf lib shard.lock && make static"
+docker run -ti --rm -v "$PWD":/app --user="$UID" markterm-builder /bin/sh -c "cd /app && rm -rf lib shard.lock && shards build --without-development --release --static"
 mv bin/markterm bin/markterm-static-linux-amd64
 
 # Build for ARM64
 docker build . -f Dockerfile.static --platform linux/arm64 -t markterm-builder
-docker run -ti --rm -v "$PWD":/app --platform linux/arm64 --user="$UID" markterm-builder /bin/sh -c "cd /app && rm -rf lib shard.lock && make static"
+docker run -ti --rm -v "$PWD":/app --platform linux/arm64 --user="$UID" markterm-builder /bin/sh -c "cd /app && rm -rf lib shard.lock && shards build --without-development --release --static"
 mv bin/markterm bin/markterm-static-linux-arm64
--- do_release.sh
+++ do_release.sh
@@ -5,11 +5,11 @@ PKGNAME=$(basename "$PWD")
 VERSION=$(git cliff --bumped-version |cut -dv -f2)
 
 sed "s/^version:.*$/version: $VERSION/g" -i shard.yml
+hace static
 git add shard.yml
 hace lint test
 git cliff --bump -o
 git commit -a -m "bump: Release v$VERSION"
 git tag "v$VERSION"
 git push --tags
-hace static
 gh release create "v$VERSION" "bin/$PKGNAME-static-linux-amd64" "bin/$PKGNAME-static-linux-arm64" --title "Release v$VERSION" --notes "$(git cliff -l -s all)"
